26633ef20ee5d53b85353e13961850deb43553fa
ijab <zhancaibao@gmail.com>
2013-04-08 22:27:10 -0400

    Add nginx.

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def run_simple_httpd_server(app, ip, port=8080):
 #
 if __name__ == '__main__':
    ip   = os.environ['OPENSHIFT_INTERNAL_IP']
-   port = 8080
+   port = 15080
    zapp = imp.load_source('application', 'wsgi/application')
 
    #  Use gevent if we have it, otherwise run a simple httpd server.
