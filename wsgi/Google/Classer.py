86331200235eac2c8bceb0296ffefec4bf029dbc
XUN XU <dx88968@gmail.com>
2013-04-21 15:57:59 -0400

    Update Classer

--- wsgi/Google/Classer.py
+++ wsgi/Google/Classer.py
@@ -5,8 +5,8 @@ class TypeIdentifier:
     def getTypeByTF(self, TF):
         return Search.get_field_by_tf(TF)
         
-    def getTypeByQuery(self, query):
-        return Search.get_field_by_query(query)
+    def getTypeByQuery(self, query, type):
+        return Search.get_field_by_query(query,type)
         
     def contains(self, type1, type2):
         return Search.get_relavance(type1, type2)
